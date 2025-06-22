813febc93173b9aad8093cd556e1ed52d908fb57
Moritz Pollert <pollertmoritz@gmail.com>
2025-06-22 14:31:30 +0200

    Updated module.ts

--- extensions/zendesk/src/module.ts
+++ extensions/zendesk/src/module.ts
@@ -39,7 +39,8 @@ export default createExtension({
     onNotFoundTicket,
 
     updateTicketNode,
-
+    attachFlowDataNode,
+    
     searchArticlesNode,
     onFoundArticles,
     onNotFoundArticles,
